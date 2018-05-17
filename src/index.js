35cd1ee5fb7c068637952e0f398a037df6a41a7b
Jeff Connelly <jeff.connelly6@gmail.com>
2018-05-16 18:48:56 -0600

    deleted wrong linter setup, moved component structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
